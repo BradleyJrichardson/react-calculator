7353dc3e6bdeaa2f80dbf25e6491d908ebb598b5
BradleyJrichardson <brdl.richardson@gmail.com>
2019-07-06 13:17:12 +1000

    dom elements created - calculator buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="calculator-display">0</div>
+      <div className="calculator-keypad">
+        <div className="input-key">
+          <div className="function-keys">
+            <button className="calculator-key key-clear" />
+            <button className="calculator-key key-sign" />
+            <button className="calculator-key key-percent" />
+          </div>
+          <div className="digit-keys">
+            <button className="calculator-key key-0" />
+            <button className="calculator-key key-dot" />
+            <button className="calculator-key key-1" />
+            <button className="calculator-key key-2" />
+            <button className="calculator-key key-3" />
+            <button className="calculator-key key-4" />
+            <button className="calculator-key key-5" />
+            <button className="calculator-key key-6" />
+            <button className="calculator-key key-7" />
+            <button className="calculator-key key-8" />
+            <button className="calculator-key key-9" />
+          </div>
+        </div>
+        <div className="operator-keys">
+          <button className="calculator-key key-divide" />
+          <button className="calculator-key key-multiply" />
+          <button className="calculator-key key-subtract" />
+          <button className="calculator-key key-add" />
+          <button className="calculator-key key-equals" />
+        </div>
+      </div>
     </div>
   );
 }
